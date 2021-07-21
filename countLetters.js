41ebbb2077081c517b186cddb106fd86c1afab6d
Gloria Edem <gloedem@gmail.com>
2021-07-21 22:20:19 +0000

    function to count the letters in a provided sentence and return the results as an object

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let trimmed = sentence.split(" ").join("");
+  //console.log(trimmed, trimmed.length);
+  let results = {};
+  for (const letter of trimmed) {
+    if (results[letter]) {
+      results[letter]++;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+console.log(countLetters("this is the last time!"));
+console.log(countLetters("I am still not fully comfortable with this"));
