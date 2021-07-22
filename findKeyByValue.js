69642542a17eb29a7a89badbeda70ef42b2d34c8
Gloria Edem <gloedem@gmail.com>
2021-07-22 00:15:50 +0000

    function to find and return the first key which contains the value provided at the function call

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//findKeyByValue should scan the object and return the first key with the given value...
+//i'm looping through an object, so i'll use the for of loop
+const findKeyByValue = function(obj, value) {
+  for (const item in obj) {
+    //console.log(item); //this returns the keys
+    if (value === obj[item]) {
+      return item;
+    }
+  }
+
+};
+
+//Test Case #1: Best TV Shows By Genre
+const bestTVShowsByGenre = {'sci_fi': 'The Expanse', comedy: 'Brooklyn Nine-Nine', drama: 'The Wire'};
+assertEqual(findKeyByValue(bestTVShowsByGenre, 'The Wire'), 'drama');
+
+//Test Case #2:
+const favSongsByArtist = {coldplay: 'In My Place', keane: 'This is the last time', coin: 'Boyfriend', travis: 'Waving at the Window' };
+assertEqual(findKeyByValue(favSongsByArtist, 'Boyfriend'), 'coin');
