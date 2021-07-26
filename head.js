e172fc39799e457993fa3d18a048a9403c6fece8
Gloria Edem <gloedem@gmail.com>
2021-07-26 16:10:53 +0000

    exported head function for use by test files in test/

--- head.js
+++ head.js
@@ -1,22 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
-/* Assert Equal Test Cases
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
-assertEqual(1, 4);
-assertEqual('Sweet', 'Bitter');
-assertEqual('Sweet', 'Sweet');
-*/
 const head = function(arr) {
 	return arr[0]
 }
-assertEqual(head([1,2,3]), 1);
-assertEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Lighthouse');
-assertEqual(head([101]), 101);
-assertEqual(head([]), undefined);
+
+module.exports = head
