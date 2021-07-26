3c9abbbb08bde29e8d83874f384b609d6cff7bea
Gloria Edem <gloedem@gmail.com>
2021-07-26 16:30:02 +0000

    refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,33 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    //first check if arrays are same length
-    return false;
-  } else {
-    //console.log(`${arr1} is the same length as ${arr2}`);
-    let counter = 0;
-    for (let i = 0; i < arr1.length; i++) {
-      // loop through each item
-      //let counter = 0;
-      if (arr1[i] === arr2[i]) {
-        // if values are same, add to accumulator (counter)
-        counter += 1;
-      }
-    } //console.log(counter); --> verification test
-    if (counter === arr1.length) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-};
+const assertEqual = require('./assertEqual')
+const eqArrays = require('./eqArrays')
 
 const assertArraysEqual = function(arr1, arr2) {
   let result = eqArrays(arr1, arr2);
@@ -38,7 +10,4 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([101, 23, 35, 49], [101, 23, 35, 49]); //shoud pass
-assertArraysEqual(["alpha", "tango", "foxtrot"], ["alpha", "echo", "foxtrot"]); //should fail
-assertArraysEqual(["alpha", "tango", "foxtrot"], ["alpha", "tango", "foxtrot"]); //should pass
-assertArraysEqual([1, 2, 3], [1, 2, "3"]); //should fail
+module.exports = assertArraysEqual
