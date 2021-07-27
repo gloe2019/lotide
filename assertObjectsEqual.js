91ada8bdb301d4ac39d4d881009d0c62f7368185
Gloria Edem <gloedem@gmail.com>
2021-07-27 01:33:53 +0000

    code cleanup...

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,50 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) { //first check if arrays are same length
-    return false;
-  } else {
-    let counter = 0;
-    for (let i = 0; i < arr1.length; i++) { // loop through each item
-      //let counter = 0;
-      if (arr1[i] === arr2[i]) { // if values are same, add to accumulator (counter)
-        counter += 1;
-      }
-    } //console.log(counter); --> verification test
-    if (counter === arr1.length) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-};
-
-const eqObjects = function(object1, object2) {
-  let obj1Keys = Object.keys(object1);
-  let obj2Keys = Object.keys(object2);
-  if (obj1Keys.length !== obj2Keys.length) {
-    return false;
-  }
-  for (const key of obj1Keys) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!eqArrays(object1[key], object2[key])) {
-        return false;
-      }
-      continue;
-    }
-    if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
-  //return `${object1} === ${object2}`;
-  
-};
+const assertEqual = require('./assertEqual')
+const eqArrays = require('./eqArrays')
+const eqObjects = require('./eqObjects')
 
 
 const assertObjectsEqual = function(actual, expected, obj1, obj2) {
@@ -60,6 +16,7 @@ const assertObjectsEqual = function(actual, expected, obj1, obj2) {
   console.log('====')
   }
 }; //
+
 let obj1 = {a: '1', b: '3'};
 let obj2 = {b: '3', a: '1'};
 let obj3 = {a: 1, b: 3};
--- eqObjects.js
+++ eqObjects.js
@@ -1,32 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) { //first check if arrays are same length
-    return false;
-  } else {
-    let counter = 0;
-    for (let i = 0; i < arr1.length; i++) { // loop through each item
-      //let counter = 0;
-      if (arr1[i] === arr2[i]) { // if values are same, add to accumulator (counter)
-        counter += 1;
-      }
-    } //console.log(counter); --> verification test
-    if (counter === arr1.length) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-};
-
-//console.log(eqArrays([1, 2, 3, 4], [4, 3, 2, 1])); //should be false
-//console.log(eqArrays(['2', 3], ['2', 3])); //should be true
-
+const assertEqual = require('./assertEqual')
+const eqArrays = require('./eqArrays')
 
 const eqObjects = function(object1, object2) {
   let obj1Keys = Object.keys(object1);
@@ -48,18 +21,18 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true); // => true
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertEqual(eqObjects(ab, ba), true); // => true
 
-// const abc = { a: "1", b: "2", c: "3" };
-// const abd = { a: "1", b: "2", d: "4" };
-// assertEqual(eqObjects(abd, abc), false); // => false
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true); // => true
+// // const abc = { a: "1", b: "2", c: "3" };
+// // const abd = { a: "1", b: "2", d: "4" };
+// // assertEqual(eqObjects(abd, abc), false); // => false
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true); // => true
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false);
 
 module.exports = eqObjects
